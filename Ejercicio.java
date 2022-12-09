7b4d533105c50757ba623105e63414a3fd91b61e
miguelfal <falconmiguel.2004@gmail.com>
2022-12-09 21:33:19 +0100

    commit

--- Ejercicio.java
+++ Ejercicio.java
@@ -2,14 +2,20 @@ import matematicas.Varias;
 
 public class Ejercicio {
     public static void main(String[] args) {
-        System.out.println(Varias.voltea(123456789));
-        int x =1234321;
-        if (Varias.esCapicua(x)){
-            System.out.println(x+ " es capicua ");
-            
-        }else{
-            System.out.print(x + " no es capicua ");
+
+        /****************** Ejercicio 2 ES PRIMO ******************/
+
+        if(Varias.esPrimo(7)){
+            System.out.println("El 7 es primo");
         }
+
+        /****************** Ejercicio 5 DIGITOS ******************/
+
+        System.out.println("El 5 volteado es "+ Varias.voltea(5));
+        System.out.println("El 4872 volteado es "+ Varias.voltea(4872));
+        System.out.println("El -18947 volteado es "+ Varias.voltea(-18947));
+
+
         
     }
 }
